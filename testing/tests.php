05a5382256273013b2d80fa55e930f85638bd6d0
Siberian <siberian@sibprojects.com>
2021-01-27 15:16:24 +0200

    - fix path

--- testing/tests.php
+++ testing/tests.php
@@ -6,7 +6,7 @@ class Tests extends PHPUnit_Framework_TestCase {
 
 	function testGitVersion() {
 
-		require_once('src/_autoload.php');
+		require_once('../src/_autoload.php');
 		$composerTree = new Src\ComposerTree();
 		$version = $composerTree->readGit('https://github.com/dnoegel/php-xdg-base-dir.git');
 
